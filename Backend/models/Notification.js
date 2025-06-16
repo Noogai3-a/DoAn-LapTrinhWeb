630125adc0c5c212fa6490e7c0e4dab1b9beeb8c
UIT-23521602 <23521602@gm.uit.edu.vn>
2025-06-16 12:05:45 +0700

    Khong deploy duoc - chua co j het

--- Backend/models/Notification.js
+++ Backend/models/Notification.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  // Người nhận thông báo
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     type: { 
       type: String, 
       enum: ['COMMENT', 'REPLY', 'APPROVE', 'REJECT'] ,
@@ -18,4 +18,5 @@ const notificationSchema = new mongoose.Schema({
     postTitle: { type: String },
     postType: { type: String, enum: ['BLOG', 'DOCUMENT'] },
   });
+  
 module.exports = mongoose.model('Notification', notificationSchema);
