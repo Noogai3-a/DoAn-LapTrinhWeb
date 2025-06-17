1df1ef2b7095013e346466411c4aa43c6e6cdc36
UIT.23521296 <23521296@gm.uit.edu.vn>
2025-06-17 22:04:33 +0700

    Sửa lọc bình luận

--- Frontend/Public/js/blog-read.js
+++ Frontend/Public/js/blog-read.js
@@ -408,7 +408,7 @@ document.addEventListener('DOMContentLoaded', async function () {
       textarea.value = '';
     } catch (err) {
       console.error('Gửi bình luận lỗi:', err);
-      alert(err.message);
+      showToast(err.msg, 'error')
     }
   });
 });
